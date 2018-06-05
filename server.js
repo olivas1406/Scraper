ef0a9360fe84095d21923e8a2addc046255614a0
olivas1406 <olivas1406@msn.com>
2018-06-04 18:19:08 -0700

    Added axiosJS and updated server.js

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 
-const 
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cheerio = require("cheerio");
+const axios = require("axios");
 
+var port = 1337;
 
+const db = require("./models");
 
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/scraperDB");
+
+app.get("/scrape"), function(req, res) {
+    axios.get("http://www.fark.com").then(function(response) {
+        var $ = cheerio.load(response.data);
+        
+    })
+}
 
 
 
