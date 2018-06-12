55dd28139140db06b1751278fccdd40e7121ed8a
Daniel <olivas1406@msn.com>
2018-06-11 20:42:49 -0700

    Updated port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs = require("express-handlebars");
 const routes = require("./controller/scraper_controller.js");
 const app = express();
 
-let port = process.env.port || 1337; 
+let PORT = process.env.port || 1337; 
 
 app.use(routes);
 
@@ -23,6 +23,6 @@ let MONGODB_URI = process.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect("mongodb://scraperUser:scrapeMe1337@ds255930.mlab.com:55930/heroku_hlxt5c6q");
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
 });
