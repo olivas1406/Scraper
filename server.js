a83622c604e985b0886a2736752c2c3a8e3a8420
Daniel <olivas1406@msn.com>
2018-06-11 20:28:40 -0700

    Updated server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.set("view engine", "handlebars");
 
 let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect("mongodb:scraperUser:scrapeMe1337@ds255930.mlab.com:55930/heroku_hlxt5c6q");
+mongoose.connect("mongodb://scraperUser:scrapeMe1337@ds255930.mlab.com:55930/heroku_hlxt5c6q");
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
